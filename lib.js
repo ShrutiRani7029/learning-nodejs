d9bbc73933e2df6f85bfbdcef108768261819766
@coder shruti <www.meshruti@gmail.com>
2023-12-19 03:55:47 +0530

    Modules, Packages

---
+++ lib.js
@@ -0,0 +1,21 @@
+
+///REQUIRE OR COMMON JS MODULE
+exports.sum=(a,b)=>{
+    return a+b;
+}
+
+exports.diff=(a,b)=>{
+    return a-b;
+}
+
+///ES MODULE (ECMA SCRIPT)
+/*const sum=(a,b)=>{
+    return a+b;
+}
+
+const diff=(a,b)=>{
+    return a-b;
+}
+
+export {sum,diff}
+*/
